ae2c1af85858ecf69f43382a8c7ab3b3eaea1031
zhiwenyan <zhiwenyan@icloud.com>
2016-12-06 13:33:50 +0800

    first commit

---
+++ src/main/java/com/steven/maven/Hello.java
@@ -0,0 +1,6 @@
+package com.steven.maven;
+public class Hello {
+	public String sayHello(String name){
+		return "Hello "+name+"!";
+	}
+}
---
+++ src/test/java/com/steven/maven/HelloTest.java
@@ -0,0 +1,11 @@
+package com.steven.maven;	
+import org.junit.Test;
+import static junit.framework.Assert.*;
+public class HelloTest {
+	@Test
+	public void testHello(){
+		Hello hello = new Hello();
+		String results = hello.sayHello("litingwei");
+		assertEquals("Hello litingwei!",results);	
+	}
+}
